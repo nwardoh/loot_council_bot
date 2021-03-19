80f407c1f07c890e495e747e0c1bcc75642fe3af
nwardoh <THZ$RMHS0@qQIWEV>
2021-03-19 10:21:25 -0500

    command test

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f'{client.user} has connected to Discord!'.format(client))
 
 client.run(TOKEN)
 
@@ -19,6 +19,6 @@ client.run(TOKEN)
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content.startswith('$hello'):
+    if message.content.startswith('*hello'):
         await message.channel.send("Hello!")
 
