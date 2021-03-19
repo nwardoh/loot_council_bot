26d36c3913048a77a673db7b51174c220dba1138
nwardoh <THZ$RMHS0@qQIWEV>
2021-03-19 11:37:04 -0500

    connect message 7

--- main.py
+++ main.py
@@ -10,18 +10,6 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 
-#client = discord.Client()
-
-#@client.event
-#async def on_ready():
-#    print(f'{client.user} has connected to Discord!')
-#client.run(TOKEN)
-
-#@bot.event
-#async def on_message(message):
-#	if message.content == 'test':
-#		await message.channel.send('Testing 1 2 3!')
-
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
