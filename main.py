0333ee5ae040580727289694c2a08ffdc5b17d17
nwardoh <THZ$RMHS0@qQIWEV>
2021-03-19 11:08:46 -0500

    command test 9

--- main.py
+++ main.py
@@ -1,34 +1,19 @@
 # bot.py
 import os
-
 import discord
-from discord.ext import commands
+from discord.ext.commands import Bot
+
+bot = commands.Bot(command_prefix="+")
 
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 
-client = discord.Client()
-
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!'.format(client))
-
-bot = commands.Bot(command_prefix="+")
-
-client.run(TOKEN)
-
-#@bot.command()
-#async def test(ctx):
-#    await message.channel.send('testing')
-
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content == 'ping':
-        await message.channel.send('pong')
+	if message.content == 'test':
+		await message.channel.send('Testing 1 2 3!')
 
 bot.run(TOKEN)
 
